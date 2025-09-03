04dd9194eca5d22949ff7955df2f0fcbafb52e76
Halfdan Sand Christensen <halfdansc@proton.me>
2025-09-03 11:09:34 +0200

    singly linked list first take

--- src/dsa/data_structures/linked_lists/singly_linked_list.zig
+++ src/dsa/data_structures/linked_lists/singly_linked_list.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const testing = std.testing;
 
 pub fn SinglyLinkedList(comptime T: type) type {
     return struct {
@@ -16,7 +17,10 @@ pub fn SinglyLinkedList(comptime T: type) type {
         }
 
         pub fn deinit(self: *Self) void {
-            _ = self;
+            while (self.popHead() != null) {
+                // pop fress nodes so keep this loop body empty
+            }
+            // don't destroy self since the list will usually be stack allocated
         }
 
         pub fn isEmpty(self: *Self) bool {
@@ -41,6 +45,44 @@ pub fn SinglyLinkedList(comptime T: type) type {
             self.allocator.destroy(poppedHead);
 
             return data;
+        }
+
+        pub fn contains(self: *Self, target_data: T) bool {
+            var current = self.head;
+            while (current) |node| {
+                if (node.data == target_data) return true;
+                current = node.next;
+            }
+            return false;
+        }
+
+        // TODO: add reverse function!
+
+        pub fn iterator(self: *Self) Iterator(self) {
+            return Iterator(self){
+                .current = self.head,
+            };
+        }
+
+        fn Iterator(self: *Self) type {
+            return struct {
+                const This = @This();
+
+                current: ?*Node(T),
+
+                pub fn reset(this: *This) void {
+                    this.current = self.head;
+                }
+
+                pub fn next(this: *This) ?T {
+                    if (this.current) |node| {
+                        const data = node.data;
+                        this.current = node.next;
+                        return data;
+                    }
+                    return null;
+                }
+            };
         }
     };
 }
--- src/dsa/lib.zig
+++ src/dsa/lib.zig
@@ -1,14 +1,24 @@
+// algorithims
 const sorting_module = @import("./algorithms/sorting.zig");
 const searching_module = @import("./algorithms/searching.zig");
 
+// data structures
+const singly_linked_list_module = @import("./data_structures/linked_lists/singly_linked_list.zig");
+
 // Ensure tests are included in compilation
 comptime {
+    // algorithms
     _ = sorting_module;
     _ = searching_module;
+
+    // data structures
+    _ = singly_linked_list_module;
 }
 
 // add data structures here
-pub const DataStructures = struct {};
+pub const DataStructures = struct {
+    pub const singly_linked_list = singly_linked_list_module.SinglyLinkedList;
+};
 
 // add algorithms here
 pub const Algorithms = struct {

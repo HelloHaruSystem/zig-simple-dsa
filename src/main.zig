a1db1cb552c67cfda60d8e2bddbe3c4e511db1d9
Halfdan Sand Christensen <halfdansc@proton.me>
2025-09-15 18:19:09 +0200

    changed main.zig to not use global variables

--- src/main.zig
+++ src/main.zig
@@ -1,11 +1,11 @@
 const std = @import("std");
 const dsa = @import("dsa");
 
-var stdout_buffer: [4096]u8 = undefined;
-var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
-const stdout = &stdout_writer.interface;
-
 pub fn main() !void {
+    var stdout_buffer: [1024]u8 = undefined;
+    var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
+    const stdout = &stdout_writer.interface;
+
     try stdout.print("Hello, {s}!\n", .{"World"});
 
     // Don't forget to flush

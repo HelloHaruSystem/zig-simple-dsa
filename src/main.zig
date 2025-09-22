a09413f40b88177241424d8610dfd33c3cd8e01f
Halfdan Sand Christensen <halfdansc@proton.me>
2025-09-22 23:21:46 +0200

    tested list in main

--- src/main.zig
+++ src/main.zig
@@ -2,9 +2,29 @@ const std = @import("std");
 const dsa = @import("dsa");
 
 pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer _ = gpa.deinit();
+    const allocator = gpa.allocator();
+
     var stdout_buffer: [2048]u8 = undefined;
     var stdout_writer = std.fs.File.stdout().writer(&stdout_buffer);
     const stdout = &stdout_writer.interface;
+
+    var list = dsa.DataStructures.singly_linked_list(u8).init(allocator);
+    defer list.deinit();
+
+    try list.prepend(2);
+    try list.prepend(4);
+    try list.prepend(8);
+    try list.prepend(16);
+    list.reverse();
+
+    var it = list.iterator();
+
+    while (it.next()) |value| {
+        try stdout.print("{d} -> ", .{value});
+    }
+    try stdout.print("null\n", .{});
 
     try stdout.print("Hello, {s}!\n", .{"World"});
 

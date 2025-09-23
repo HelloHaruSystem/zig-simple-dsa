201b2caad77b57cd43d022eaa566e74d8a327519
Halfdan Sand Christensen <halfdansc@proton.me>
2025-09-23 20:53:28 +0200

    sort method

--- src/dsa/data_structures/linked_lists/doubly_linked_list.zig
+++ src/dsa/data_structures/linked_lists/doubly_linked_list.zig
@@ -62,6 +62,20 @@ pub fn DoublyLinkedList(comptime T: type) type {
             self.size += 1;
         }
 
+        pub fn peekHead(self: *Self) ?T {
+            if (self.head) |head| {
+                return head.value;
+            }
+            return null;
+        }
+
+        pub fn peekTail(self: *Self) ?T {
+            if (self.tail) |tail| {
+                return tail.value;
+            }
+            return null;
+        }
+
         pub fn popHead(self: *Self) ?T {
             if (self.isEmpty()) return null;
 
@@ -110,11 +124,101 @@ pub fn DoublyLinkedList(comptime T: type) type {
             }
         }
 
+        pub fn reverse(self: *Self) void {
+            if (self.isEmpty()) return;
+
+            var current = self.head;
+
+            while (current) |node| {
+                std.mem.swap(?*Node(T), &node.next, &node.prev);
+
+                // move to next node (prev is now the next after swap)
+                current = node.prev;
+            }
+
+            // swap head and tal
+            std.mem.swap(?*Node(T), &self.head, &self.tail);
+        }
+
+        pub fn sort(self: *Self) !void {
+            if (self.size <= 1) return;
+
+            const n = self.size;
+            const mid_index = n / 2;
+
+            // split list
+            var left = try self.createSubList(0, mid_index);
+            var right = try self.createSubList(mid_index, n);
+            defer left.deinit();
+            defer right.deinit();
+
+            // divide
+            try left.sort();
+            try right.sort();
+
+            // conquer
+            try self.merge(&left, &right);
+        }
+
         // helper functions
         fn createNode(self: *Self, value: T) !*Node(T) {
             const newNode = try self.allocator.create(Node(T));
             newNode.* = Node(T).init(value);
             return newNode;
+        }
+
+        // merge sort helper function
+        fn createSubList(self: *Self, start_index: usize, end_index: usize) !Self {
+            var new_list = Self.init(self.allocator);
+
+            if (start_index >= self.size or start_index >= end_index) {
+                return new_list;
+            }
+
+            var current = self.head;
+            var i: usize = 0;
+
+            // skip to start index
+            while (current != null and i < start_index) : (i += 1) {
+                current = current.?.next;
+            }
+
+            // copy elements over to the new list
+            while (current != null and i < end_index) : (i += 1) {
+                try new_list.append(current.?.value);
+                current = current.?.next;
+            }
+
+            return new_list;
+        }
+
+        // merge sort helper function
+        fn merge(self: *Self, left: *Self, right: *Self) !void {
+            self.clear();
+
+            var l_current = left.head;
+            var r_current = right.head;
+
+            while (l_current != null and r_current != null) {
+                if (l_current.?.value <= r_current.?.value) {
+                    try self.append(l_current.?.value);
+                    l_current = l_current.?.next;
+                } else {
+                    try self.append(r_current.?.value);
+                    r_current = r_current.?.next;
+                }
+            }
+
+            // add remaining
+            while (l_current != null) {
+                try self.append(l_current.?.value);
+                l_current = l_current.?.next;
+            }
+
+            while (r_current != null) {
+                try self.append(r_current.?.value);
+                r_current = r_current.?.next;
+            }
         }
     };
 }

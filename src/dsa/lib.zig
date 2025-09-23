eb6ff4d1530d269db4f38c8c91ee47ae38a034b1
Halfdan Sand Christensen <halfdansc@proton.me>
2025-09-23 15:19:36 +0200

    added doubly linked list to the correct directory

--- src/dsa/lib.zig
+++ src/dsa/lib.zig
@@ -4,6 +4,7 @@ const searching_module = @import("./algorithms/searching.zig");
 
 // data structures
 const singly_linked_list_module = @import("./data_structures/linked_lists/singly_linked_list.zig");
+const doubly_linked_list_module = @import("./data_structures/linked_lists/doubly_linked_list.zig");
 
 // Ensure tests are included in compilation
 comptime {
@@ -13,11 +14,13 @@ comptime {
 
     // data structures
     _ = singly_linked_list_module;
+    _ = doubly_linked_list_module;
 }
 
 // add data structures here
 pub const DataStructures = struct {
     pub const singly_linked_list = singly_linked_list_module.SinglyLinkedList;
+    pub const doubly_linked_list = doubly_linked_list_module.DoublyLinkedList;
 };
 
 // add algorithms here

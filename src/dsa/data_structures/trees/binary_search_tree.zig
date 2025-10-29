ef98c043916d880a168ab597e3cb446ec54a9d1d
Halfdan Sand Christensen <halfdansc@proton.me>
2025-10-29 14:43:03 +0100

    BST insert function recursively and iteratively

--- src/dsa/data_structures/trees/binary_search_tree.zig
+++ src/dsa/data_structures/trees/binary_search_tree.zig
@@ -25,7 +25,7 @@ pub fn BinarySearchTree(comptime T: type) type {
 
         // BST fields
         allocator: std.mem.Allocator,
-        root: *?Node,
+        root: ?*Node,
         size: usize,
 
         pub fn init(allocator: std.mem.Allocator) Self {
@@ -34,6 +34,73 @@ pub fn BinarySearchTree(comptime T: type) type {
                 .root = null,
                 .size = 0,
             };
+        }
+
+        // TODO: Implement:
+        // make a generic post-order function to handle the traversal logic
+        pub fn deinit(self: *Self) void {
+            _ = self;
+        }
+
+        pub fn insertIterative(self: *Self, value: T) !void {
+            if (self.root == null) {
+                self.root = try self.createNode(value);
+                return;
+            }
+
+            var current = self.root.*;
+            // while current is not null
+            while (current) |node| {
+                // if less than current
+                if (value < node.value) {
+                    if (node.left == null) {
+                        node.left = try self.createNode(value);
+                        return;
+                    } else {
+                        current = node.left;
+                    }
+                } else {
+                    // if higher than current
+                    if (node.right == null) {
+                        node.right = try self.createNode(value);
+                        return;
+                    } else {
+                        current = node.right;
+                    }
+                }
+            }
+        }
+
+        pub fn insertRecursively(self: *Self, value: T) !void {
+            self.root = try self.insertRecursivelyHelper(self.root, value);
+        }
+
+        pub fn insertRecursivelyHelper(self: *Self, node: ?*Node, value: T) !?*Node {
+            // base case
+            if (node == null) {
+                return try self.createNode(value);
+            }
+
+            // unwrap
+            const current_node = node.?;
+
+            // recursive step
+            if (value < current_node.value) {
+                current_node.left = try self.insertRecursivelyHelper(current_node.left, value);
+            } else {
+                current_node.right = try self.insertRecursivelyHelper(current_node.right, value);
+            }
+
+            return node;
+        }
+
+        // TODO: implement getMax(), getMin() and contains
+
+        // Helper functions
+        fn createNode(self: *Self, value: T) !*Node {
+            const new_node = try self.allocator.create(Node);
+            new_node.* = Node.init(value);
+            return new_node;
         }
     };
 }
